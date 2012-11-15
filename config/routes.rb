c1584baf893ba318e5344a131806eefd02d2b634
Rik Matena <rik.matena@eduhub.nl>
2012-11-15 01:35:11 +0100

    On feed page: added link to user's profile on user's full name & gravatar; named profile route to be able to do this.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Treebook::Application.routes.draw do
   get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
 
-  get '/:id', to: 'profiles#show'
+  get '/:id', to: 'profiles#show', as: :profile
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
